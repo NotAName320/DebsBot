4b0d460cfc40d2d3ee131ac99ef907c6a9dc202e
Ray Chen <rc144@outlook.com>
2020-08-31 21:30:00 -0700

    added basic functions

---
+++ DebsBot.py
@@ -0,0 +1,35 @@
+import os
+
+import discord
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print(f'{client.user} connected')
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('db!'):
+        command = message.content.replace("db!", "")
+        params = command.split()
+        if command.startswith('bill'):
+            try:
+                response = requests.get(params[1])
+                embedDesc = "Choice 1 - 0 votes\n\nChoice 2 - 0 votes\n\nChoice 3 - 0 votes"
+                billEmbed = discord.Embed(title="Vote on Bill", description=embedDesc, color=16711680, url=params[1])
+                await message.channel.send('Please vote on "' + message.embeds[0].title + '"', embed=billEmbed)
+            except requests.ConnectionError as exception:
+                await message.channel.send('Not a valid URL.')
+        else:
+            await message.channel.send('Incorrect command. db!help for a list of available commands.')
+
+
+client.run(TOKEN)
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # DebsBot
  A Discord bot for the Combined Syndicates of America server.
+
+[Add the bot](https://discord.com/api/oauth2/authorize?client_id=750193408781451264&permissions=1073881200&scope=bot)
